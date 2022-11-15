34c549c97c93d0dae582dfd6ca5b6048a17bb001
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-11-15 22:02:57 +0100

    build: update deploy script

--- genetic-presentation/deploy.sh
+++ genetic-presentation/deploy.sh
@@ -1,14 +1,6 @@
 # abort on errors
 set -e
 
-# clear output
-rm dist/bundle.js
-
-# build
-npx webpack
-
-# navigate into the build output directory
-cd dist
 git init
 git add -A
 git commit -m 'deploy through script'
